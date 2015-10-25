363091c3bae0603ee53b1e28f259e7d34abd2639
Devon Walshe <devon.walshe@gmail.com>
2015-10-25 11:17:21 +0000

    added two variables to model, Age_group and Cabin, no significant change in model performance

--- model_predict.R
+++ model_predict.R
@@ -24,14 +24,37 @@ all_data$Fare[1044] <- median(all_data$Fare, na.rm = TRUE)
 predicted_age <- rpart(Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked + Title,
                        data = all_data[!is.na(all_data$Age),], method = "anova")
 all_data$Age[is.na(all_data$Age)] <- predict(predicted_age, all_data[is.na(all_data$Age),])
+## place ages in bins
+all_data$Age_group = cut(all_data$Age, 
+                         breaks = c(-Inf, 18, 25, 30, 35, 40, 45, 50, 55, Inf), 
+                         labels = c("Child", "18-25", "26-30", "31-35", "36-40", "41-45", "46-50", "51-55", "55+"), 
+                         right=FALSE)
+
+#scrub cabin numbers
+all_data$Cabin = gsub('[[:digit:]]+', '', all_data$Cabin)
+all_data$Cabin = gsub(' ', '', all_data$Cabin)
+all_data$Cabin = gsub('([[:alpha:]])\\1+', '\\1', all_data$Cabin)
+all_data$Cabin = gsub('FG', '', all_data$Cabin)
+all_data$Cabin = gsub('FE', '', all_data$Cabin)
+all_data$Cabin = gsub('T', '', all_data$Cabin)
+
+## Predict cabin floor
+predicted_cabin = rpart(Cabin ~ Pclass + Sex + SibSp + Parch + Fare + Embarked + Title,
+                        data = all_data[all_data$Cabin != "",], method = "class")
+cabin = predict(predicted_cabin, all_data[all_data$Cabin == "",], type="class")
+all_data[all_data$Cabin == "",]$Cabin = as.factor(unlist(sapply(cabin, as.character)))
+all_data$Cabin = as.factor(all_data$Cabin)
+
 # Split datasets again
 train <- all_data[1:891,]
 test <- all_data[892:1309,]
 # Train random forest model
-titanic_forest = randomForest(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + Fare + Title, data=train, method="class", ntree = 2000, importance=TRUE)
+titanic_forest = randomForest(as.factor(Survived) ~ Pclass + Sex + Cabin + Age_group + SibSp + Fare + Title, data=train, method="class", ntree = 100, importance=TRUE)
 # Predict on test data and write out
 titanic_pred = predict(titanic_forest, test)
+# varImpPlot(titanic_forest)
+
 titanic_forest_csv = data.frame(PassengerId = test$PassengerId, Survived = titanic_pred)
-write.csv(titanic_forest_csv, "forest_pred.csv", row.names=FALSE)
+write.csv(titanic_forest_csv, "forest_pred2.csv", row.names=FALSE)
 
 
